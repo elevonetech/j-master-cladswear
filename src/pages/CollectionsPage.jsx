c2b4ada9a6da7230bd5124055ec039cdb71ca01e
mukuvi <jamesmngandu@gmail.com>
2026-07-24 22:41:41 +0300

    commit

--- src/pages/CollectionsPage.jsx
+++ src/pages/CollectionsPage.jsx
@@ -420,17 +420,6 @@ export function CollectionsPage() {
           </div>
         </div>
       </section>
-
-      {/* WhatsApp FAB */}
-      <a
-        href="https://wa.me/254702364786"
-        target="_blank"
-        rel="noreferrer"
-        aria-label="Chat on WhatsApp"
-        className="fixed bottom-6 right-6 z-50 flex h-14 w-14 items-center justify-center rounded-full bg-[#25D366] text-white shadow-[0_8px_32px_rgba(37,211,102,0.4)] transition-transform hover:scale-105"
-      >
-        <MessageCircle size={24} />
-      </a>
     </div>
   );
 }
